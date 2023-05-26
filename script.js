92f5aa96a3965bd09a841b21617616d211b050f8
Himanshu Aggarwal <120503620+HimanshuAggarwal1404@users.noreply.github.com>
2023-05-26 07:09:14 +0530

    carousel functioning added
     text sliding up

--- script.js
+++ script.js
@@ -3,15 +3,60 @@ function open() {
   document.getElementsByClassName("menu").style.transform = "scale(0)";
 }
 function moveCarousel() {
+  for (var i = 0; i <= 3; i++) {
+    document.getElementsByClassName("text1")[
+      i
+    ].style.transform = `translateY(40vh)`;
+  }
+  for (var i = 0; i <= 3; i++) {
+    document.getElementsByClassName("text2")[
+      i
+    ].style.transform = `translateY(40vh)`;
+  }
+  for (var i = 0; i <= 3; i++) {
+    document.getElementsByClassName("text3")[
+      i
+    ].style.transform = `translateY(40vh)`;
+  }
+  for (var i = 0; i <= 3; i++) {
+    document.getElementsByClassName("text4")[
+      i
+    ].style.transform = `translateY(40vh)`;
+  }
+
   if (carouselposition >= -300) {
-    carouselposition = carouselposition - 100;}
-    else{
-        carouselposition=0;
+    carouselposition = carouselposition - 100;
+  } else {
+    carouselposition = 0;
+  }
+  if (carouselposition == 0) {
+    for (var i = 0; i <= 3; i++) {
+      document.getElementsByClassName("text1")[
+        i
+      ].style.transform = `translateY(0vh)`;
     }
-    document.getElementById(
-      "carousel"
-    ).style.transform = `translateX(${carouselposition}vw)`;
-    console.log(carouselposition);
-  
+  } else if (carouselposition == -100) {
+    for (var i = 0; i <= 3; i++) {
+      document.getElementsByClassName("text2")[
+        i
+      ].style.transform = `translateY(0vh)`;
+    }
+  } else if (carouselposition == -200) {
+    for (var i = 0; i <= 3; i++) {
+      document.getElementsByClassName("text3")[
+        i
+      ].style.transform = `translateY(0vh)`;
+    }
+  } else if (carouselposition == -300) {
+    for (var i = 0; i <= 3; i++) {
+      document.getElementsByClassName("text4")[
+        i
+      ].style.transform = `translateY(0vh)`;
+    }
+  } else pos = 5;
+  document.getElementById(
+    "carousel"
+  ).style.transform = `translateX(${carouselposition}vw)`;
+  console.log(carouselposition);
 }
-setInterval(moveCarousel, 3000);
+setInterval(moveCarousel, 6000);
